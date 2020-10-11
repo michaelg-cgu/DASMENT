64a3e89f9f332b756e1909b48aa71ca7b19215b5
miketg <michael.garcia@cgu.edu>
2020-10-10 20:12:49 -0700

    Added User authentication, Added user verification, Added simple User login profile page. Profile page will include graphs. And other form. Added bootstraps, added login form. next section is building out user requirements per team input

--- FoodforThought/FoodforThought/urls.py
+++ FoodforThought/FoodforThought/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include  # include adds list to url patterns
 from myusers import views as user_views
 
@@ -23,5 +24,8 @@ from myusers import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='myusers/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='myusers/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
--- FoodforThought/myusers/views.py
+++ FoodforThought/myusers/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Accound create for {username}! ')
-            return redirect('blog-home')
+            messages.success(request, f'Your Account has been Create! You are now able to login')
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'myusers/register.html', {'form': form})
+
+# the @ adds functionality to the component below
+@login_required
+def profile(request):
+    return render(request, 'myusers/profile.html')
