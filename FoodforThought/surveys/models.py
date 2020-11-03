adab2c009f4c80031559a20d94c1b01de65d7460
devenpl <deven.panchal@cgu.edu>
2020-11-03 10:31:16 -0800

    Attempted to fix all rebase issues

--- FoodforThought/surveys/models.py
+++ FoodforThought/surveys/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from myusers.models import Profile
 
 allergyList = [
     ('none', 'None'),
@@ -9,28 +11,33 @@ allergyList = [
     ('nuts', 'Nuts')
 ]
 
-reactionList = [
+severityChoices = [
     ('none', 'None'),
     ('mild', 'Mild'),
     ('moderate', 'Moderate'),
     ('severe', 'Severe'),
-    ('toxic', 'Toxic')
+    ('deadly', 'Deadly')
 ]
 
 #Food Allergies
-class Allergy(models.Model):
+class FoodAllergy(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     allergy1 = models.CharField(max_length=20, default='None', choices=allergyList)
+    reaction1 = models.CharField(max_length=20, default='None', choices=severityChoices)
     allergy2 = models.CharField(max_length=20, default='None', choices=allergyList)
+    reaction2 = models.CharField(max_length=20, default='None', choices=severityChoices)
     allergy3 = models.CharField(max_length=20, default='None', choices=allergyList)
+    reaction3 = models.CharField(max_length=20, default='None', choices=severityChoices)
     allergy4 = models.CharField(max_length=20, default='None', choices=allergyList)
+    reaction4 = models.CharField(max_length=20, default='None', choices=severityChoices)
     allergy5 = models.CharField(max_length=20, default='None', choices=allergyList)
+    reaction5 = models.CharField(max_length=20, default='None', choices=severityChoices)
 
-class Reaction(models.Model):
-    reaction1 = models.CharField(max_length=20, default='None', choices=reactionList)
-    reaction2 = models.CharField(max_length=20, default='None', choices=reactionList)
-    reaction3 = models.CharField(max_length=20, default='None', choices=reactionList)
-    reaction4 = models.CharField(max_length=20, default='None', choices=reactionList)
-    reaction5 = models.CharField(max_length=20, default='None', choices=reactionList)
+    def __str__(self):
+        return f'{User.username} Profile'
+
+    def save(self, *args, **kawargs):
+        super(FoodAllergy, self).save()  # run the save method of the parent
 
 class OtherAllergyQuestion(models.Model):
     question = models.CharField(max_length=20)
@@ -47,15 +54,9 @@ environmentalChoices = [
     ('insect bites', 'Insect Bites')
 ]
 
-severityChoices = [
-    ('none', 'None'),
-    ('mild', 'Mild'),
-    ('moderate', 'Moderate'),
-    ('severe', 'Severe'),
-    ('deadly', 'Deadly')
-]
 
 class Environmental(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     eallergy1 = models.CharField(max_length=20, default='None', choices=environmentalChoices)
     severity1 = models.CharField(max_length=20, default='None', choices=severityChoices)
     eallergy2 = models.CharField(max_length=20, default='None', choices=environmentalChoices)
@@ -66,3 +67,9 @@ class Environmental(models.Model):
     severity4 = models.CharField(max_length=20, default='None', choices=severityChoices)
     eallergy5 = models.CharField(max_length=20, default='None', choices=environmentalChoices)
     severity5 = models.CharField(max_length=20, default='None', choices=severityChoices)
+
+    def __str__(self):
+        return f'{User.username} Profile'
+
+    def save(self, *args, **kawargs):
+        super(Environmental, self).save()  # run the save method of the parent
--- FoodforThought/surveys/views.py
+++ FoodforThought/surveys/views.py
@@ -2,52 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .forms import testingForm, AllergyForm, ReactionForm, EnvironmentalForm  # , #QuestionForm
+from .forms import  AllergyForm, EnvironmentalForm  #testingForm, QuestionForm
 
 
 @login_required
 def surveys(request):
 
     if request.method == 'POST':
-        form = testingForm(request.POST)
-        if form.is_valid():
-            allergy1 = form.cleaned_data['Allergy_1']
-            allergy2 = form.cleaned_data['Allergy_2']
-            allergy3 = form.cleaned_data['Allergy_3']
-            allergy4 = form.cleaned_data['Allergy_4']
-            allergy5 = form.cleaned_data['Allergy_5']
-
-            reaction1 = form.cleaned_data['Reaction_1']
-            reaction2 = form.cleaned_data['Reaction_2']
-            reaction3 = form.cleaned_data['Reaction_3']
-            reaction4 = form.cleaned_data['Reaction_4']
-            reaction5 = form.cleaned_data['Reaction_5']
-
-            form.save()
-
-    form = testingForm()
-    return render(request, 'surveys/foodallergies.html', {'form': form})
-
-
-def allergy_detail(request):
-
-    if request.method == 'POST':
         form = AllergyForm(request.POST)
         if form.is_valid():
+            #allergy1 = form.cleaned_data['Allergy_1']
+            #allergy2 = form.cleaned_data['Allergy_2']
+            #allergy3 = form.cleaned_data['Allergy_3']
+            #allergy4 = form.cleaned_data['Allergy_4']
+            #allergy5 = form.cleaned_data['Allergy 5']
+
+            #reaction1 = form.cleaned_data['Reaction_1']
+            #reaction2 = form.cleaned_data['Reaction_2']
+            #reaction3 = form.cleaned_data['Reaction_3']
+            #reaction4 = form.cleaned_data['Reaction_4']
+            #reaction5 = form.cleaned_data['Reaction_5']
+
             form.save()
+            messages.success(request, f'Your Account has been Create! You are now able to login')
+            return redirect('profile')
 
     form = AllergyForm()
-    return render(request, 'surveys/foodallergies.html', {'form': form})
-
-
-def reaction_detail(request):
-
-    if request.method == 'POST':
-        form = ReactionForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-    form = ReactionForm()
     return render(request, 'surveys/foodallergies.html', {'form': form})
 
 
@@ -58,6 +38,8 @@ def environmental_detail(request):
         eform = EnvironmentalForm(request.POST)
         if eform.is_valid():
             eform.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
 
     eform = EnvironmentalForm()
     return render(request, 'surveys/enviromentalallergies.html', {'eform': eform})
